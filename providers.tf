8b0c2a1c613c4a74e21899f5322528448614dac2
Fabien RENAUD <frd.frenaud@gmail.com>
2025-01-24 09:52:13 +0100

    Revert "fix: github provider credentials"
    This reverts commit ce8e1175a1812aaf326e38f34a4b87fb8b9bdb23.

--- providers.tf
+++ providers.tf
@@ -1,8 +1,4 @@
 provider "github" {
   owner = var.github_organization.name
-  app_auth {
-    id              = var.github_app_id
-    installation_id = var.github_app_installation_id
-    pem_file        = var.github_app_pem_file
-  }
+  app_auth {}
 }
